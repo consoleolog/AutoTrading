7dc9ac17dafaf1d62f6e1386d6f6d0cf9847bd9c
fergus <leopportunity5@gmail.com>
2025-01-23 21:07:58 +0900

    error fix

--- model/dto/trading_result.py
+++ model/dto/trading_result.py
@@ -4,9 +4,11 @@ class TradingResult:
         self.ticker = trading_result['ticker']
         self.mode = trading_result['mode']
         self.peekout = trading_result['peekout']
-        self.increase = trading_result['increase']
         self.cross_signal = trading_result['cross_signal']
-        self.decrease = trading_result['decrease']
+        if trading_result['increase'] is not None:
+            self.increase = trading_result['increase']
+        if trading_result['decrease'] is not None:
+            self.decrease = trading_result['decrease']
 
     def __str__(self):
         return f"""
